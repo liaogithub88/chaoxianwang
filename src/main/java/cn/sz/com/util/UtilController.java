e238cf7d463cf5396dc1398f249a6663b6485822
liaogithub88 <543700808@qq.com>
2019-08-16 14:26:03 +0800

    初始化

---
+++ src/main/java/cn/sz/com/util/UtilController.java
@@ -0,0 +1,14 @@
+package cn.sz.com.util;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class UtilController {
+	@RequestMapping("/")
+	public String showFirstPage() {
+		
+		return "client/index";
+	}
+}
